d24e2a731b6db3fefbcb2acf5c675e4853ba0189
Ejjonzey <ejjonzey1982@gmail.com>
2019-12-06 22:09:42 -0500

    parameters and arguments

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,11 +1,30 @@
 // console.log('Hello there');
 // function definition
-function calculateBill() {
+
+function calculateBill(billAmount, taxRate = 0.07, tipRate = 0.2) {
   // this is the function body
   // console.log('running calculate bill!');
-  const total = 100 * 1.07;
+  const total = billAmount * taxRate * tipRate;
   // console.log(total);
   return total;
 }
 // function call.  Or **run**
-const myTotal = calculateBill();
+const myTotal = calculateBill(100, 0.07);
+
+function sayHiTo(firstName) {
+  return `Hello ${firstName}`;
+}
+
+// const greeting = sayHiTo('Bosco');
+// console.log(greeting);
+
+// const myTotal3 = calculateBill(20 + 15 + 33 + 52, 0.07);
+
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+
+// can set default arguments to empty type to not throw error.
+function yell(name = '') {
+  return `Hey ${name.toUpperCase()}`;
+}
