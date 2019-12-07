8a2455b9c394c8b4238f3e7951428cb958dcbedb
Ejjonzey <ejjonzey1982@gmail.com>
2019-12-06 21:29:08 -0500

    custom functions

---
+++ playground/custom-functions/cf.js
@@ -0,0 +1,11 @@
+// console.log('Hello there');
+// function definition
+function calculateBill() {
+  // this is the function body
+  // console.log('running calculate bill!');
+  const total = 100 * 1.07;
+  // console.log(total);
+  return total;
+}
+// function call.  Or **run**
+const myTotal = calculateBill();
