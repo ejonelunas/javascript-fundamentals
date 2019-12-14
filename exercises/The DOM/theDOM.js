c01dc1e31cd7ba15aa05d66b8bbdf6cdd655a354
Ejjonzey <ejjonzey1982@gmail.com>
2019-12-14 09:19:31 -0500

    selecting elements, element properties and methods

---
+++ exercises/The DOM/theDOM.js
@@ -0,0 +1,23 @@
+// // const p = document.querySelector('p');
+// // console.log(p);
+// // const imgs = document.querySelectorAll('.item img');
+// // console.log(imgs);
+// // const item2 = document.querySelector('.item2');
+// // const item2Image = item2.querySelector('img');
+// const heading = document.querySelector('h2');
+// // set the textContent property on selected element
+// // heading.textContent = 'Eric is 20 days into 100 days of code';
+// console.log(heading.textContent);
+// console.log(heading.innerText);
+// // textContent selects all elements, including script and style tags
+// // innerText only shows human readable elements.
+// console.log(heading.innerHTML);
+
+// const tangerineList = document.querySelector('.Tangerine');
+// console.log(tangerineList.textContent);
+
+// tangerineList.insertAdjacentText('beforeend', '🍊 ');
+
+// Classes
+const pic = document.querySelector('.bigUn');
+console.log(pic.classList);
